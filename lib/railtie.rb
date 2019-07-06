480782ed4c95ba28db10443f9867f31409f83643
Kevin Forest <k.forest@hdenergie.ca>
2019-07-06 15:28:51 -0400

    Fix error ternary

--- lib/railtie.rb
+++ lib/railtie.rb
@@ -34,7 +34,7 @@ module  ActiveOrient
       ActiveOrient.database = databaseyml[Rails.env.to_sym] || 'temp'
 
        # don't allocate models if no file is provided (by default)
-       ActiveOrient::Model.keep_models_without_file = configyml[:keep_models_without_file].present? configyml[:keep_models_without_file] : false
+       ActiveOrient::Model.keep_models_without_file = configyml[:keep_models_without_file].present? ? configyml[:keep_models_without_file] : false
 
        if connectyml.present? and connectyml[:user].present? and connectyml[:pass].present?
         ActiveOrient.default_server= { user: connectyml[:user], password: connectyml[:pass] ,
